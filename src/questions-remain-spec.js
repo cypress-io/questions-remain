a40399b925144a52830954aab1cbbd31fa690c65
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-11 10:52:10 -0400

    first test passed

--- src/questions-remain-spec.js
+++ src/questions-remain-spec.js
@@ -4,9 +4,25 @@
 const questionsRemain = require('.')
 const la = require('lazy-ass')
 const is = require('check-more-types')
+const snapshot = require('snap-shot')
 
 describe('@cypress/questions-remain', () => {
   it('is a function', () => {
     la(is.fn(questionsRemain))
+  })
+
+  it('returns object if all questions have been answered', () => {
+    const dontAsk = () => {
+      throw new Error('Should not ask!')
+    }
+    const propertiesToQuestions = {
+      foo: dontAsk,
+      bar: dontAsk
+    }
+    const options = {
+      foo: 'foo is specified',
+      bar: 'so is bar'
+    }
+    return snapshot(questionsRemain(propertiesToQuestions)(options))
   })
 })
