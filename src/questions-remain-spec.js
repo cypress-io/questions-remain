a8e53bd9712797e247a7d091599b48f708a287f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-11 10:58:11 -0400

    tested calling supplied function to provide an answer

--- src/questions-remain-spec.js
+++ src/questions-remain-spec.js
@@ -5,16 +5,18 @@ const questionsRemain = require('.')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const snapshot = require('snap-shot')
+const sinon = require('sinon')
 
 describe('@cypress/questions-remain', () => {
+  const dontAsk = () => {
+    throw new Error('Should not ask!')
+  }
+
   it('is a function', () => {
     la(is.fn(questionsRemain))
   })
 
   it('returns object if all questions have been answered', () => {
-    const dontAsk = () => {
-      throw new Error('Should not ask!')
-    }
     const propertiesToQuestions = {
       foo: dontAsk,
       bar: dontAsk
@@ -24,5 +26,22 @@ describe('@cypress/questions-remain', () => {
       bar: 'so is bar'
     }
     return snapshot(questionsRemain(propertiesToQuestions)(options))
+  })
+
+  it('asks questions for missing options', () => {
+    const barStub = sinon.stub().resolves('bar user answer')
+    const propertiesToQuestions = {
+      foo: dontAsk,
+      bar: barStub
+    }
+    const options = {
+      foo: 'foo is specified'
+      // notice bar is missing!
+    }
+    return snapshot(
+      questionsRemain(propertiesToQuestions)(options)
+    ).then(answers => {
+      la(barStub.called, 'bar stub has not been called')
+    })
   })
 })
