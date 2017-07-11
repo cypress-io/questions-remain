591710e01735ecbfafce550af7c96e9e75da8635
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-11 10:36:45 -0400

    initial

---
+++ src/questions-remain-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const questionsRemain = require('.')
+
+describe('@cypress/questions-remain', () => {
+  it('write this test', () => {
+    console.assert(questionsRemain, 'should export something')
+  })
+})
